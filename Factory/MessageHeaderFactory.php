ff58e8fa188657f8825a5eb050940b775d49d7b1
Sören Helbig <soerenhelbig@gmail.com>
2014-01-10 15:25:44 +0100

    removes obsolete methods in MessageHeaderFactory

--- Factory/MessageHeaderFactory.php
+++ Factory/MessageHeaderFactory.php
@@ -18,64 +18,7 @@ class MessageHeaderFactory
      */
     public function byRawContent($header)
     {
-//        $headerArray = $this->explodeMessageHeaderText($header);
-//        $headerArray = $this->getArrayWithKeys($headerArray);
-
         return $this->byArray(iconv_mime_decode_headers($header));
-    }
-
-    /**
-     * @param string $headerText
-     * @return array
-     */
-    private function explodeMessageHeaderText($headerText)
-    {
-        $tmpHeader = explode("\r\n",$headerText);
-        $header = array();
-
-        foreach ($tmpHeader as $field) {
-            if (substr($field,0,1) !== "\t" && substr($field,0,1) !== ' ') {
-                $header[] = $field;
-            } else {
-                $header[count($header)-1] = $this->appendToLastField($header, $field);
-            }
-        }
-
-        return $header;
-    }
-
-    /**
-     * @param array $headers
-     * @param string $rawValue
-     * @return string
-     */
-    private function appendToLastField(array $headers, $rawValue)
-    {
-        $value = preg_replace(array('/\s{2,}/', '/\t/'), ' ', $rawValue);
-
-        $headerField = end($headers);
-        $headerField = $headerField . ' ;' . $value;
-
-        return $headerField;
-    }
-
-    /**
-     * @param array $headerArray
-     * @return array
-     */
-    private function getArrayWithKeys($headerArray)
-    {
-        $target = array();
-
-        foreach ($headerArray as $field) {
-            preg_match('/^([A-Za-z0-9\-]+):(.+)$/', $field, $matches);
-
-            if (count($matches) === 3) {
-                $target[$matches[1]] = trim($matches[2]);
-            }
-        }
-
-        return $target;
     }
 
     /**
